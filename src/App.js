1ddd9c59351eacec2330576fcafb5401922eefaa
minsu kim <kimminsu31415@gmail.com>
2024-01-20 20:25:02 +0900

    tailwind 적용 완료!

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-function App() {
-  return (
-    <div class="container px-5 py-24 mx-auto flex flex-wrap">
-     <div class="h-full p-4 lg:w-1/3">
-       <div class=" bg-gray-100 px-8 pt-16 pb-16 relative">
-         <h2 class="text-xs mb-1">CATEGORY</h2>
-         <h1 class="title-font sm:text-2xl text-xl  mb-3">Raclette Blueberry Nextious</h1>
-         <p class="mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-         <a class="text-indigo-500 inline-flex items-center">Learn More</a>
-       </div>
-     </div>
-     <div class="h-full p-4 lg:w-1/3">
-       <div class="bg-gray-100 px-8 pt-16 pb-16 relative">
-         <h2 class="text-xs mb-1">CATEGORY</h2>
-         <h1 class="title-font sm:text-2xl text-xl  mb-3">Ennui Snackwave Thundercats</h1>
-         <p class="mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-         <a class="text-indigo-500 inline-flex items-center">Learn More</a>
-       </div>
-     </div>
-     <div class="h-full p-4 lg:w-1/3">
-       <div class="bg-gray-100 px-8 pt-16 pb-16 relative">
-         <h2 class="text-xs mb-1">CATEGORY</h2>
-         <h1 class="title-font sm:text-2xl text-xl mb-3">Selvage Poke Waistcoat</h1>
-         <p class="mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-         <a class="text-indigo-500 inline-flex items-center">Learn More</a>
-       </div>
-     </div>
-   </div>
-  );
-}
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomeView from "./views/HomeView";
+
+const App = () => {
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomeView />} />
+        </Routes>
+    </BrowserRouter>
+    );
+};
 
 export default App;
